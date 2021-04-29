4a4fafec4aa90ebedd41baaa20a1ca8a5c880055
unknown <anirudhmandana@gmail.com>
2021-04-29 19:55:09 +0530

    Second Commit

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ from sklearn.neural_network import MLPClassifier
 app = Flask(__name__)
 
 ## Configure upload location for audio
-app.config['UPLOAD_FOLDER'] = "./audio"
+app.config['UPLOAD_FOLDER'] = "./static/audio"
 
 ## Route for home page
 @app.route('/')
 def home():
-    return render_template('index.html',value="")
+    return render_template('index.html',value="No Audio")
 
 
 ## Route for results
@@ -27,26 +27,29 @@ def results():
     This route is used to save the file, convert the audio to 16000hz monochannel,
     and predict the emotion using the saved binary model
     """
-    if not os.path.isdir("./audio"):
-        os.mkdir("audio")
+    if not os.path.isdir("./static/audio"):
+        os.mkdir("static/audio")
     if request.method == 'POST':
         try:
+          if len(os.listdir("./static/audio"))!=0:
+            wav_file_pre  = os.listdir("./static/audio")[0]
+            wav_file_pre = f"{os.getcwd()}\\static\\audio\\{wav_file_pre}"
+            os.remove(wav_file_pre)
           f = request.files['file']
           filename = secure_filename(f.filename)
           f.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
           print("File Saved "+filename)
         except:
-          return render_template('index.html', value="")
+          return render_template('index.html', value="No Audio",f="")
 
-    wav_file_pre  = os.listdir("./audio")[0]
-    wav_file_pre = f"{os.getcwd()}\\audio\\{wav_file_pre}"
+    wav_file_pre  = os.listdir("./static/audio")[0]
+    wav_file_pre = f"{os.getcwd()}\\static\\audio\\{wav_file_pre}"
     wav_file = convert(wav_file_pre)
     check,sr = librosa.load(wav_file_pre ,res_type='kaiser_fast')
     print(wav_file_pre)
-    os.remove(wav_file_pre)
     mfcc_exmp = np.mean(librosa.feature.mfcc(y=check, sr=sr, n_mfcc=40).T,axis=0)
     model=keras.models.load_model('hinds2_model.h5')
-    observed_emotions = ['neutral','angry','happy','fearful','sad']
+    observed_emotions = ['Neutral','Angry','Happy','Fearful','Sad']
     mfcc_exmp = np.expand_dims(mfcc_exmp, axis=0)
     mfcc_exmp = np.expand_dims(mfcc_exmp, axis=2)
     pred = model.predict_classes(mfcc_exmp)
@@ -55,5 +58,5 @@ def results():
 #    x_test =extract_feature(wav_file)
 #    y_pred=model.predict(np.array([x_test]))
     #os.remove(wav_file_pre)
-    return render_template('index.html', value=observed_emotions[pred[0]])#y_pred[0])
+    return render_template('index.html', value=observed_emotions[pred[0]],f=filename)#y_pred[0])
     print(y_pred)
